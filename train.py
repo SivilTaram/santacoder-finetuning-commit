9d8bcda42dd9d300bd364245d1cd234dfb605117
SivilTaram <qianlxc@outlook.com>
2023-02-19 20:12:15 +0800

    Apply some specific changes to Github commit message fine-tuning over SantCoder.

--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 """
-Fine-Tune SantaCoder on code/text dataset
+Fine-Tune SantaCoder on Github commits diff dataset
 """
 
 import argparse
@@ -18,22 +18,27 @@ from transformers import (
     logging,
     set_seed,
 )
+from difflib import SequenceMatcher
+import ghdiff
+from functools import partial
 
 
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--model_path", type=str, default="bigcode/santacoder")
-    parser.add_argument("--dataset_name", type=str, default="bigcode/the-stack-dedup")
+    parser.add_argument("--dataset_name", type=str, default="bigcode/github-commits-diff")
     parser.add_argument("--subset", type=str, default="data")
     parser.add_argument("--split", type=str, default="train")
-    parser.add_argument("--size_valid_set", type=int, default=4000)
+    parser.add_argument("--cache_dir", type=str, default="/tmp/github-commits-diff")
+    parser.add_argument("--size_valid_set", type=int, default=5000)
     parser.add_argument("--streaming", action="store_true")
     parser.add_argument("--shuffle_buffer", type=int, default=5000)
-    parser.add_argument("--data_column", type=str, default="content")
+    parser.add_argument("--data_column", type=str, default="new_contents")
 
-
-    parser.add_argument("--seq_length", type=int, default=1024)
+    parser.add_argument("--max_input_length", type=int, default=2048)
+    parser.add_argument("--max_output_length", type=int, default=1024)
     parser.add_argument("--max_steps", type=int, default=10000)
+    parser.add_argument("--message_min_token", type=int, default=5)
     parser.add_argument("--batch_size", type=int, default=2)
     parser.add_argument("--gradient_accumulation_steps", type=int, default=8)
     parser.add_argument("--eos_token_id", type=int, default=49152)
@@ -44,18 +49,18 @@ def get_args():
     parser.add_argument("--weight_decay", type=float, default=0.05)
 
     parser.add_argument("--local_rank", type=int, default=0)
-    parser.add_argument("--no_fp16", action="store_false")
-    parser.add_argument("--no_gradient_checkpointing", action="store_false")
+    parser.add_argument("--fp16", action="store_false")
+    parser.add_argument("--gradient_checkpointing", action="store_false")
     parser.add_argument("--seed", type=int, default=0)
     parser.add_argument("--num_workers", type=int, default=None)
-    parser.add_argument("--output_dir", type=str, default="./checkpoints")
+    parser.add_argument("--output_dir", type=str, default="/tmp/checkpoints")
     parser.add_argument("--log_freq", default=1, type=int)
     parser.add_argument("--eval_freq", default=1000, type=int)
     parser.add_argument("--save_freq", default=1000, type=int)
     return parser.parse_args()
 
 
-def chars_token_ratio(dataset, tokenizer, data_column, nb_examples=400):
+def chars_token_ratio(dataset, tokenizer, data_column, nb_examples=500):
     """
     Estimate the average number of characters per token in the dataset.
     """
@@ -67,68 +72,48 @@ def chars_token_ratio(dataset, tokenizer, data_column, nb_examples=400):
     return total_characters / total_tokens
 
 
-class ConstantLengthDataset(IterableDataset):
+def get_too_common_message(dataset, nb_examples=10_0000):
     """
-    Iterable dataset that returns constant length chunks of tokens from stream of text files.
-        Args:
-            tokenizer (Tokenizer): The processor used for proccessing the data.
-            dataset (dataset.Dataset): Dataset with text files.
-            infinite (bool): If True the iterator is reset after dataset reaches end else stops.
-            seq_length (int): Length of token sequences to return.
-            num_of_sequences (int): Number of token sequences to keep in buffer.
-            chars_per_token (int): Number of characters per token used to estimate number of tokens in text buffer.
+    Perform statistics on the git messages
     """
+    common_message = {}
+    for idx, message in enumerate(dataset["message"]):
+        if idx > nb_examples:
+            break
+        if message in common_message:
+            common_message[message] += 1
+        else:
+            common_message[message] = 1
+    common_message = {k: v for k, v in common_message.items() if v > 5}
+    return set(common_message.keys())
 
-    def __init__(
-        self,
-        tokenizer,
-        dataset,
-        infinite=False,
-        seq_length=1024,
-        num_of_sequences=1024,
-        chars_per_token=3.6,
-        content_field="content",
-    ):
-        self.tokenizer = tokenizer
-        self.concat_token_id = (
-            tokenizer.eos_token_id if tokenizer.eos_token_id else args.eos_token_id
-        )
-        self.dataset = dataset
-        self.seq_length = seq_length
-        self.infinite = infinite
-        self.current_size = 0
-        self.max_buffer_size = seq_length * chars_per_token * num_of_sequences
-        self.content_field = content_field
 
-    def __iter__(self):
-        iterator = iter(self.dataset)
-        more_examples = True
-        while more_examples:
-            buffer, buffer_len = [], 0
-            while True:
-                if buffer_len >= self.max_buffer_size:
-                    break
-                try:
-                    buffer.append(next(iterator)[self.content_field])
-                    buffer_len += len(buffer[-1])
-                except StopIteration:
-                    if self.infinite:
-                        iterator = iter(self.dataset)
-                    else:
-                        more_examples = False
-                        break
-            tokenized_inputs = self.tokenizer(buffer, truncation=False)["input_ids"]
-            all_token_ids = []
-            for tokenized_input in tokenized_inputs:
-                all_token_ids.extend(tokenized_input + [self.concat_token_id])
-            for i in range(0, len(all_token_ids), self.seq_length):
-                input_ids = all_token_ids[i : i + self.seq_length]
-                if len(input_ids) == self.seq_length:
-                    self.current_size += 1
-                    yield {
-                        "input_ids": torch.LongTensor(input_ids),
-                        "labels": torch.LongTensor(input_ids),
-                    }
+tokenizer = AutoTokenizer.from_pretrained("bigcode/santacoder")
+
+
+def gh_diff(example):
+    example["gh_diff"] = ghdiff.diff(example["old_contents"], example["new_contents"])
+    return example
+
+
+def preprocess_function(examples, args):
+    # input as old content + message, output as code edits
+    inputs = [message + " : " + old_content
+              for message, old_content in zip(examples["message"], examples["old_contents"])]
+    targets = examples["gh_diff"]
+
+    model_inputs = tokenizer(inputs,
+                             max_length=args.max_input_length,
+                             padding=False,
+                             truncation=True)
+    # Tokenize targets with text_target=...
+    labels = tokenizer(text_target=targets,
+                       max_length=args.max_output_length,
+                       padding=False,
+                       truncation=True)
+
+    model_inputs["labels"] = labels["input_ids"]
+    return model_inputs
 
 
 def create_datasets(tokenizer, args):
@@ -139,7 +124,49 @@ def create_datasets(tokenizer, args):
         use_auth_token=True,
         num_proc=args.num_workers if not args.streaming else None,
         streaming=args.streaming,
+        cache_dir=args.cache_dir,
+        data_files="diffs_55574529_56098816.jsonl"
     )
+
+    chars_per_token_content = chars_token_ratio(dataset, tokenizer, "old_contents")
+    chars_per_token_message = chars_token_ratio(dataset, tokenizer, "message")
+    print(f"The character to token ratio of the content is: {chars_per_token_content:.2f}")
+    print(f"The character to token ratio of the message is: {chars_per_token_message:.2f}")
+
+    common_message = get_too_common_message(dataset)
+
+    # filter all datasets which are not positive
+    def valid_filter_dataset(example):
+        return example["new_contents"] != example["old_contents"] and \
+               len(example["new_contents"].strip()) > 0 and \
+               len(example["old_contents"].strip()) > 0 and \
+               example["message"] not in common_message and \
+               len(example["old_contents"]) < 50_000
+
+    # filter the dataset
+    dataset = dataset.filter(valid_filter_dataset,
+                             num_proc=args.num_workers)
+
+    dataset = dataset.map(gh_diff,
+                          num_proc=args.num_workers)
+
+    # filter all datasets which are not positive
+    def length_filter_dataset(example):
+        # exclude pull requests message
+        return len(example["old_contents"] + example["message"]) <= chars_per_token_content * args.max_input_length and \
+               len(example["gh_diff"]) <= chars_per_token_content * args.max_output_length and \
+               len(example["message"]) <= chars_per_token_message * args.message_min_token
+
+    def prefix_filter_dataset(example):
+        return not example["message"].startswith("Merge pull request") and \
+               not example["message"].startswith("Merge branch") and \
+               not example["message"].startswith("Bump version")
+
+    # filter the dataset
+    dataset = dataset.filter(length_filter_dataset,
+                             num_proc=args.num_workers)\
+        .filter(prefix_filter_dataset, num_proc=args.num_workers)
+
     if args.streaming:
         print("Loading the dataset in streaming mode")
         valid_data = dataset.take(args.size_valid_set)
@@ -152,23 +179,23 @@ def create_datasets(tokenizer, args):
         print(
             f"Size of the train set: {len(train_data)}. Size of the validation set: {len(valid_data)}"
         )
-    chars_per_token = chars_token_ratio(train_data, tokenizer, args.data_column)
-    print(f"The character to token ratio of the dataset is: {chars_per_token:.2f}")
-    train_dataset = ConstantLengthDataset(
-        tokenizer,
-        train_data,
-        infinite=True,
-        seq_length=args.seq_length,
-        chars_per_token=chars_per_token,
-        content_field=args.data_column,
+
+    column_names = dataset.column_names["train"]
+    train_dataset = train_data.map(
+        partial(preprocess_function, args=args),
+        batched=True,
+        num_proc=args.num_workers,
+        remove_columns=column_names,
+        load_from_cache_file=True,
+        desc="Running tokenizer on train dataset",
     )
-    valid_dataset = ConstantLengthDataset(
-        tokenizer,
-        valid_data,
-        infinite=False,
-        seq_length=args.seq_length,
-        chars_per_token=chars_per_token,
-        content_field=args.data_column,
+    valid_dataset = valid_data.map(
+        partial(preprocess_function, args=args),
+        batched=True,
+        num_proc=args.num_workers,
+        remove_columns=column_names,
+        load_from_cache_file=True,
+        desc="Running tokenizer on validation dataset",
     )
     return train_dataset, valid_dataset
 
@@ -179,7 +206,7 @@ def run_training(args, train_data, val_data):
     model = AutoModelForCausalLM.from_pretrained(
         args.model_path,
         trust_remote_code=True,
-        use_cache=not args.no_gradient_checkpointing,
+        use_cache=not args.gradient_checkpointing,
     )
     train_data.start_iteration = 0
 
@@ -200,7 +227,7 @@ def run_training(args, train_data, val_data):
         warmup_steps=args.num_warmup_steps,
         gradient_accumulation_steps=args.gradient_accumulation_steps,
         gradient_checkpointing=args.no_gradient_checkpointing,
-        fp16=args.no_fp16,
+        fp16=args.fp16,
         weight_decay=args.weight_decay,
         run_name=f"santacoder-{args.subset}",
         report_to="wandb",
@@ -224,7 +251,6 @@ def main(args):
 
 
 if __name__ == "__main__":
-
     args = get_args()
     set_seed(args.seed)
     os.makedirs(args.output_dir, exist_ok=True)
