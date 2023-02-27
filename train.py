315ea9fb5d62c41a45cf8b7091abf66b8c790a9b
SivilTaram <qianlxc@outlook.com>
2023-02-27 13:29:28 +0800

    Support streaming mode for Github-commit-diff dataset.

--- train.py
+++ train.py
@@ -4,6 +4,7 @@ Fine-Tune SantaCoder on Github commits diff dataset
 
 import argparse
 import os
+import re
 from functools import partial
 import difflib
 from datasets import load_dataset
@@ -18,6 +19,10 @@ from transformers import (
     logging,
     set_seed,
 )
+import torch
+from torch.utils.data import IterableDataset
+
+hash_regex = re.compile(r"^[a-fA-F0-9]+(?:-[a-fA-F0-9]+)*$")
 
 
 def get_args():
@@ -26,7 +31,7 @@ def get_args():
     parser.add_argument("--dataset_name", type=str, default="bigcode/github-commits-diff")
     parser.add_argument("--subset", type=str, default="data")
     parser.add_argument("--split", type=str, default="train")
-    parser.add_argument("--cache_dir", type=str, default="/tmp/github-commits-diff")
+    parser.add_argument("--cache_dir", type=str, default="/tmp/santacoder-github-commit")
     parser.add_argument("--size_valid_set", type=int, default=5000)
     parser.add_argument("--streaming", action="store_true")
     parser.add_argument("--shuffle_buffer", type=int, default=5000)
@@ -47,7 +52,7 @@ def get_args():
     parser.add_argument("--fp16", action="store_true")
     parser.add_argument("--bf16", action="store_true")
     parser.add_argument("--gradient_checkpointing", action="store_true")
-    parser.add_argument("--seed", type=int, default=0)
+    parser.add_argument("--seed", type=int, default=42)
     parser.add_argument("--num_workers", type=int, default=None)
     parser.add_argument("--output_dir", type=str, default="/tmp/checkpoints")
     parser.add_argument("--deepspeed", type=str, default=None)
@@ -70,7 +75,7 @@ def chars_token_ratio(dataset, tokenizer, data_column, nb_examples=1000):
     return total_characters / total_tokens
 
 
-def get_too_common_message(dataset, nb_examples=10_0000):
+def get_too_common_message(dataset, nb_examples=50_0000):
     """
     Perform statistics on the git messages
     """
@@ -89,20 +94,48 @@ def get_too_common_message(dataset, nb_examples=10_0000):
     return set(common_message.keys())
 
 
-def subsample_java():
-    pass
+def preprocess_message(example):
+    lines = [line.strip() for line in example["message"].splitlines() if line.strip()]
+    clean_message = ""
+    for line in lines:
+        if "git-svn-id" in line:
+            pass
+        else:
+            clean_message += line + " "
+    example["message"] = clean_message
+
+
+def compute_extend_distribution(examples):
+    extend_dict = {}
+    counter = 0
+    for example in examples:
+        if counter > 50000:
+            break
+        suffix = extract_extend(example)["extend"]
+        if suffix not in extend_dict:
+            extend_dict[suffix] = 0
+        extend_dict[suffix] += 1
+        counter += 1
+    # order extend_dict
+    sorted_dict = {k: v for k, v in sorted(extend_dict.items(), key=lambda item: item[1])}
+    print(sorted_dict)
 
 
 def commit_diff(example):
     old_lines = example["old_contents"].splitlines()
     new_lines = example["new_contents"].splitlines()
 
-    diff_lines = (line for line in difflib.unified_diff(old_lines, new_lines) if
-                  line.startswith("+") or line.startswith("-"))
+    diff_lines = [line for line in difflib.unified_diff(old_lines, new_lines) if
+                  line.startswith("+") or line.startswith("-")]
     result = ""
     for line in diff_lines[2:]:
         result += line + "\n"
     example["diff"] = result
+    return example
+
+
+def extract_extend(example):
+    example["extend"] = example["new_file"].split(".")[-1]
     return example
 
 
@@ -111,14 +144,65 @@ tokenizer.pad_token = tokenizer.eos_token
 tokenizer.sep_token = tokenizer.eos_token
 
 
+class GithubCommitDataset(IterableDataset):
+    """
+    Iterable dataset that returns constant length chunks of tokens from stream of text files.
+        Args:
+            tokenizer (Tokenizer): The processor used for proccessing the data.
+            dataset (dataset.Dataset): Dataset with text files.
+            infinite (bool): If True the iterator is reset after dataset reaches end else stops.
+            seq_length (int): Length of token sequences to return.
+            num_of_sequences (int): Number of token sequences to keep in buffer.
+            chars_per_token (int): Number of characters per token used to estimate number of tokens in text buffer.
+    """
+
+    def __init__(
+            self,
+            tokenizer,
+            dataset,
+            infinite=False,
+            seq_length=2048
+    ):
+        self.tokenizer = tokenizer
+        self.concat_token_id = (
+            tokenizer.eos_token_id if tokenizer.eos_token_id else args.eos_token_id
+        )
+        self.dataset = dataset
+        self.seq_length = seq_length
+        self.infinite = infinite
+
+    def __iter__(self):
+        iterator = iter(self.dataset)
+        more_examples = True
+        example_input = ""
+        while more_examples:
+            try:
+                example = next(iterator)
+                example_input = "Please follow the following instruction to modify the code: {message}\n\n{old_contents}\n{eos} {diff_content}".format(
+                    message=example["message"].strip(), old_contents=example["old_contents"],
+                    eos=self.tokenizer.eos_token, diff_content=example["diff"])
+            except StopIteration:
+                if self.infinite:
+                    iterator = iter(self.dataset)
+                else:
+                    more_examples = False
+                    break
+            model_inputs = self.tokenizer(example_input,
+                                          max_length=self.seq_length,
+                                          padding="max_length",
+                                          truncation=True)
+            model_inputs["labels"] = model_inputs["input_ids"].copy()
+            yield model_inputs
+
+
 def preprocess_function(examples, args):
     # input as old content + message, output as code edits
-    inputs = ["Please follow the following instruction to modify the code:" + message + "\n\n" + old_content +
+    inputs = ["Please follow the following instruction to modify the code: " + message + "\n\n" + old_content +
               tokenizer.eos_token + diff_content
               for message, old_content, diff_content in zip(examples["message"],
                                                             examples["old_contents"],
                                                             examples["diff"])]
-
+    # print(inputs[0])
     model_inputs = tokenizer(inputs,
                              max_length=args.max_input_length,
                              padding="max_length",
@@ -136,7 +220,7 @@ def create_datasets(args):
         num_proc=args.num_workers if not args.streaming else None,
         streaming=args.streaming,
         cache_dir=args.cache_dir,
-        # data_files="diffs_55574529_56098816.jsonl"
+        # data_files="diffs_58720257_59244544.jsonl"
     )
 
     chars_per_token_content = chars_token_ratio(dataset, tokenizer, "old_contents")
@@ -149,19 +233,10 @@ def create_datasets(args):
     # filter all datasets which are not positive
     def valid_filter_dataset(example):
         return example["new_contents"] != example["old_contents"] and \
-               len(example["old_contents"]) < 50_000 and \
+               len(example["old_contents"]) < 100_000 and \
                len(example["new_contents"].strip()) > 0 and \
-               len(example["old_contents"].strip()) > 0
-
-    print(f"The size of dataset is: {len(dataset):.2f}")
-    # filter the dataset
-    dataset = dataset.filter(valid_filter_dataset,
-                             num_proc=args.num_workers)
-
-    print(f"The size of dataset after valid_filtering is: {len(dataset):.2f}")
-
-    dataset = dataset.map(commit_diff,
-                          num_proc=args.num_workers)
+               not hash_regex.match(example["message"])
+        # example["message"] not in common_message and \
 
     # filter all datasets which are not positive
     def length_filter_dataset(example):
@@ -173,51 +248,73 @@ def create_datasets(args):
     def prefix_filter_dataset(example):
         return not example["message"].startswith("Merge pull request") and \
                not example["message"].startswith("Merge branch") and \
-               not example["message"].startswith("Bump version")
+               not example["message"].startswith("Bump version") and \
+               not example["message"] in ["Update contributing.md", "Update README.md"]
 
-    # filter the dataset
-    dataset = dataset.filter(length_filter_dataset,
-                             num_proc=args.num_workers)
+    if not args.streaming:
+        print(f"The size of dataset is: {len(dataset):.2f}")
+        dataset = dataset.filter(valid_filter_dataset,
+                                 num_proc=args.num_workers)
+        print(f"The size of dataset after valid_filtering is: {len(dataset):.2f}")
+        dataset = dataset.map(commit_diff,
+                              num_proc=args.num_workers)
+        # filter the dataset
+        dataset = dataset.filter(length_filter_dataset,
+                                 num_proc=args.num_workers)
+        print(f"The size of dataset after length_filtering is: {len(dataset):.2f}")
 
-    print(f"The size of dataset after length_filtering is: {len(dataset):.2f}")
-
-    dataset = dataset.filter(prefix_filter_dataset,
-                             num_proc=args.num_workers)
-
-    print(f"The size of dataset after prefix_filtering is: {len(dataset):.2f}")
+        dataset = dataset.filter(prefix_filter_dataset,
+                                 num_proc=args.num_workers)
+        print(f"The size of dataset after prefix_filtering is: {len(dataset):.2f}")
+    else:
+        dataset = dataset.filter(valid_filter_dataset)
+        dataset = dataset.map(commit_diff)
+        # filter the dataset
+        dataset = dataset.filter(length_filter_dataset)
+        dataset = dataset.filter(prefix_filter_dataset)
 
     if args.streaming:
         print("Loading the dataset in streaming mode")
         valid_data = dataset.take(args.size_valid_set)
         train_data = dataset.skip(args.size_valid_set)
         train_data = train_data.shuffle(buffer_size=args.shuffle_buffer, seed=args.seed)
+        train_dataset = GithubCommitDataset(
+            tokenizer=tokenizer,
+            dataset=train_data,
+            infinite=True,
+            seq_length=args.max_input_length
+        )
+        valid_dataset = GithubCommitDataset(
+            tokenizer=tokenizer,
+            dataset=valid_data,
+            infinite=False,
+            seq_length=args.max_input_length
+        )
     else:
-        dataset = dataset.train_test_split(test_size=0.005, seed=args.seed)
+        dataset = dataset.train_test_split(test_size=0.001, seed=args.seed)
         train_data = dataset["train"]
         valid_data = dataset["test"]
         print(
             f"Size of the train set: {len(train_data)}. Size of the validation set: {len(valid_data)}"
         )
-    for i in range(10):
-        print(train_data[i])
+        column_names = dataset.column_names["train"]
+        train_dataset = train_data.map(
+            partial(preprocess_function, args=args),
+            batched=True,
+            num_proc=args.num_workers,
+            remove_columns=column_names,
+            load_from_cache_file=True,
+            desc="Running tokenizer on train dataset",
+        )
+        valid_dataset = valid_data.map(
+            partial(preprocess_function, args=args),
+            batched=True,
+            num_proc=args.num_workers,
+            remove_columns=column_names,
+            load_from_cache_file=True,
+            desc="Running tokenizer on validation dataset",
+        )
 
-    column_names = dataset.column_names["train"]
-    train_dataset = train_data.map(
-        partial(preprocess_function, args=args),
-        batched=True,
-        num_proc=args.num_workers,
-        remove_columns=column_names,
-        load_from_cache_file=True,
-        desc="Running tokenizer on train dataset",
-    )
-    valid_dataset = valid_data.map(
-        partial(preprocess_function, args=args),
-        batched=True,
-        num_proc=args.num_workers,
-        remove_columns=column_names,
-        load_from_cache_file=True,
-        desc="Running tokenizer on validation dataset",
-    )
     return train_dataset, valid_dataset
 
 
@@ -252,8 +349,8 @@ def run_training(args, train_data, val_data):
         bf16=args.bf16,
         deepspeed=args.deepspeed,
         weight_decay=args.weight_decay,
-        run_name=f"santacoder-{args.subset}",
-        report_to="wandb"
+        run_name=args.output_dir,
+        # report_to="wandb"
     )
     # Data collator
     data_collator = DataCollatorWithPadding(
